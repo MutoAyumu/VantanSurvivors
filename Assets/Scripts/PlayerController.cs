277c9a9f47aea4a5c4c1e7d67c4f116c798ec27a
MutoAyumu <mutoayum0610@gmail.com>
2022-05-26 04:15:41 +0900

    [add]とりあえずの素材を追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
 
     PlayerManager _playerManager;
 
-    private void Start()
+    private void Awake()
     {
         _playerManager = PlayerManager.Instance;
         _playerManager.SetPlayer(this);
@@ -18,8 +18,23 @@ public class PlayerController : MonoBehaviour
         var h = Input.GetAxisRaw("Horizontal");
         var v = Input.GetAxisRaw("Vertical");
 
+        Flip(h);
+
         transform.position += new Vector3(h * _speed * Time.deltaTime, v * _speed * Time.deltaTime, 0);
 
         _playerManager.Skill.ForEach(s => s.Update());
+    }
+    void Flip(float h)
+    {
+        if (h > 0)
+        {
+
+            this.transform.localScale = new Vector3(Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+        }
+        else if (h < 0)
+        {
+
+            this.transform.localScale = new Vector3(-1 * Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+        }
     }
 }
